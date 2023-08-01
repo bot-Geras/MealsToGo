698e50cde87a7bd975cb3d5136800a3fbd8523f2
Brian <briandave771@gmail.com>
2023-08-02 00:57:20 +0300

    Optimize Spacer Component

--- src/features/restaurants/components/restaurants-info-card.component.js
+++ src/features/restaurants/components/restaurants-info-card.component.js
@@ -72,10 +72,12 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
                 CLOSED TEMPORARILY
               </Text>
             )}
-           <Spacer variant={"left.large"} />
+           <Spacer position='left' size='large' >
             {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
-            <Spacer variant={"left.large"} />
-            <Image style={{ width: 15, height: 15 }} source={icon} />
+            </Spacer >
+            <Spacer position='left' size='large' >
+            <Image style={{ width: 15, height: 15 }} source={{uri: icon}} />
+            </Spacer>
           </SectionEnd>
         </Section>
 
