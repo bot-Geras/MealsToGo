3a14fa084e9876062aa5fdfc0b8af95595173224
Brian <briandave771@gmail.com>
2023-08-02 03:43:17 +0300

    Add Tab Icons

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import {  useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { SafeArea } from "./src/components/utils/safe-area.component";
+import { Ionicons, Entypo } from '@expo/vector-icons';
 const Tab = createBottomTabNavigator()
 
 const SettingScreen = () => { return <SafeArea><Text>hi</Text></SafeArea>}
@@ -30,7 +31,32 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+          screenOptions={
+            ({route}) => ({
+              tabBarIcon: ({focused, color, size}) => {
+                let iconName
+                if(route.name === "Restaurant") {
+                  iconName = "md-restaurant"
+                   
+                } else if(route.name === "Map") {
+                  iconName = "map"
+                    
+                } else if(route.name === "Settings") {
+                  iconName = "settings"
+                    
+                }
+
+                return <Ionicons name={iconName} size={size} color={color} />
+              }
+            })
+          }
+          tabBarOptions={{
+            activeTintColor: "tomato",
+            inactiveTintColor: "gray",
+          }}
+        
+        >
           <Tab.Screen name="Restaurant" component={RestaurantsScreen} />
           <Tab.Screen name="Map" component={MapScreen} />
           <Tab.Screen name="Settings" component={SettingScreen} />
