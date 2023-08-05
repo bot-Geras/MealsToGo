146b7c053ad5d6101ca036dfe32aab2912cfcf11
Brian <briandave771@gmail.com>
2023-08-05 20:44:27 +0300

    Simplify Tab Icon

--- App.js
+++ App.js
@@ -4,17 +4,53 @@ import { Text } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
-import {  useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald';
-import {  useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { SafeArea } from "./src/components/utils/safe-area.component";
-import { Ionicons, Entypo } from '@expo/vector-icons';
-const Tab = createBottomTabNavigator()
+import { Ionicons, Entypo } from "@expo/vector-icons";
+const Tab = createBottomTabNavigator();
+const TAB_ICON = {
+  Restaurants: "ios-restaurant",
+  Maps: "ios-map",
+  Settings: "settings",
+};
+const SettingScreen = () => {
+  return (
+    <SafeArea>
+      <Text>hi</Text>
+    </SafeArea>
+  );
+};
+const MapScreen = () => {
+  return (
+    <SafeArea>
+      <Text>hi</Text>
+    </SafeArea>
+  );
+};
 
-const SettingScreen = () => { return <SafeArea><Text>hi</Text></SafeArea>}
-const MapScreen = () => { return <SafeArea><Text>hi</Text></SafeArea>}
-
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+    tabBarActiveTintColor: "tomato",
+    tabBarInactiveTintColor: "gray",
+    tabBarStyle: [
+      {
+        display: "flex",
+      },
+      null,
+    ],
+  };
+};
+import { restaurantsRequest } from "./src/services/restaurants/restaurants.service";
 export default function App() {
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
@@ -31,36 +67,11 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={
-            ({route}) => ({
-              tabBarIcon: ({focused, color, size}) => {
-                let iconName
-                if(route.name === "Restaurant") {
-                  iconName = "md-restaurant"
-                   
-                } else if(route.name === "Map") {
-                  iconName = "map"
-                    
-                } else if(route.name === "Settings") {
-                  iconName = "settings"
-                    
-                }
-
-                return <Ionicons name={iconName} size={size} color={color} />
-              }
-            })
-          }
-          tabBarOptions={{
-            activeTintColor: "tomato",
-            inactiveTintColor: "gray",
-          }}
-        
-        >
-          <Tab.Screen name="Restaurant" component={RestaurantsScreen} />
-          <Tab.Screen name="Map" component={MapScreen} />
-          <Tab.Screen name="Settings" component={SettingScreen} />
-        </Tab.Navigator>
+          <Tab.Navigator screenOptions={createScreenOptions}>
+            <Tab.Screen name="Restaurant" component={RestaurantsScreen} />
+            <Tab.Screen name="Map" component={MapScreen} />
+            <Tab.Screen name="Settings" component={SettingScreen} />
+          </Tab.Navigator>
         </NavigationContainer>
       </ThemeProvider>
 
--- src/features/restaurants/components/restaurants-info-card.component.js
+++ src/features/restaurants/components/restaurants-info-card.component.js
@@ -37,8 +37,8 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
         <Text variant="label">{name}</Text>
         <Section>
           <Rating>
-            {ratingArray.map(() => (
-              <SvgXml xml={star} width={20} height={20} />
+            {ratingArray.map((i) => (
+              <SvgXml xml={star} width={20} height={20} key={i}/>
             ))}
           </Rating>
           <SectionEnd>
