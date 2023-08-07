cb2b4016a57d50990b74d93658c6fffc34825dc7
Brian <briandave771@gmail.com>
2023-08-07 23:59:56 +0300

    wip

--- src/services/restaurants/mock/index.js
+++ src/services/restaurants/mock/index.js
@@ -2,14 +2,12 @@ import antwerp from "./antwerp.json";
 import chicago from "./chicago.json";
 import toronto from "./toronto.json";
 import san_francisco from "./san_francisco.json";
-
 export const mocks = {
   "51.219448,4.402464": antwerp,
   "43.653225,-79.383186": toronto,
   "41.878113,-87.629799": chicago,
   "37.7749295,-122.4194155": san_francisco,
 };
-
 export const mockImages = [
   "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
   "https://www.foodiesfeed.com/wp-content/uploads/2019/04/mae-mu-oranges-ice-600x750.jpg",
