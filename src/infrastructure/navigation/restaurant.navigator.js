5244641f9f211120b14a3f44045e68624e301c34
Brian <briandave771@gmail.com>
2023-08-10 00:44:11 +0300

    Modal and Touch Feedback

--- src/infrastructure/navigation/restaurant.navigator.js
+++ src/infrastructure/navigation/restaurant.navigator.js
@@ -1,12 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
+import { createStackNavigator,  TransitionPresets } from '@react-navigation/stack'
 import { RestaurantsScreen } from '../../features/restaurants/screens/restaurants.screen'
 const RestaurantStack = createStackNavigator()
 export const RestaurantNavigator = () => {
   return (
     <RestaurantStack.Navigator screenOptions={{
         headerShown: false,
+        ...TransitionPresets.ModalPresentationIOS,
     }}>
         <RestaurantStack.Screen 
             name='Restaurants'
